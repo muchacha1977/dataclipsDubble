8102ea53056e773c242efe67a23910ab2f693022
Thorsten <tlubinski@yahoo.de>
2014-01-16 04:16:15 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dataclips::Application.config.secret_key_base = '055098a7f1a8b4c92c42639f16631c7c63915bb8863ad88265bedccf778b5179708ed69b80925c1220aa428c915093bdce89c863d7801fa3e5454486c0c8fe14'
