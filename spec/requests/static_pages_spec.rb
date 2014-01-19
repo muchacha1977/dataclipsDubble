ecc2ab5e6e6f315794574dacdc970e829515300c
maryna.varavina@googlemail.com <maryna.varavina@googlemail.de>
2014-01-20 00:02:06 +0100

    Add two static pages (About, Contact) Add two new routeres add two rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,15 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end 
   end
+
+describe "About page" do
+    before { visit about path }
+	  it { should have selector('h1', text: 'About') }
+	  it { should have selector('title', text: full_title('About us')) }
+	end
+describe "Contact page" do
+	before { visit contact path }
+ 		it { should have selector('h1', text: 'Contact') }
+		it { should have selector('title', text: full_title('Contact')) }
+	end
 end
