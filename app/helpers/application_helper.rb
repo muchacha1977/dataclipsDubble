93b19151190bf9915e086d2e324e86d8cef52b89
tlubinski <tlubinski@yahoo.de>
2014-01-24 23:04:21 -0800

    fixed a few things in layout
    - different nav-bars for login and logout
    - show selected nav item
    - included css for login-page
    - fixed wrong form-tags in login-page
    - removed bootstrap from application.js (it’s loaded by template)
    - empty custom.css.scss
    - removed breadcrumbs from login page
    - removed unify-logo from nav-bar
    - fixed urls for template-js
    - removed top-bar from layout
    - nav-bar with gray background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def getClassValueForNavItem(navItemControllerName, controllerName)
+		p "Controller: " + controllerName + "==" + controllerName
+		return "active" if navItemControllerName == controllerName
+		return ""
+	end
 end
